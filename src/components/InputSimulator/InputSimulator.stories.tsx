8503f9b3faca4b881944fd82875d0de801250436
yuriBaza23 <yuribaza2321@gmail.com>
2022-05-30 01:25:55 -0300

    InputSimulator stories

---
+++ src/components/InputSimulator/InputSimulator.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import InputSimulator from "./InputSimulator";
+import '../../styles/tailwind.css';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "AngelUI/InputSimulator/Main",
+  component: InputSimulator,
+} as ComponentMeta<typeof InputSimulator>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof InputSimulator> = (args) => <InputSimulator {...args}>{args.children}</InputSimulator>;
+
+export const MainInputSimulator = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+MainInputSimulator.args = {
+  children: 'I am an input simulator',
+};
