19f5f22e234183f7b12163488bd1be15f5732a67
YouMustPh <you@example.comphps6200@gmail.com>
2022-05-30 18:47:38 -0300

    bannerComponent plus responsividade

--- src/components/BannerMkt/BannerMkt.tsx
+++ src/components/BannerMkt/BannerMkt.tsx
@@ -5,18 +5,20 @@ export interface IBannerMktProps extends HTMLAttributes<HTMLDivElement> {
   width?: string;
   height?: string;
   fontFamily?: string;
+  fontSize?: string;
 }
 
 const BannerMkt = ({
-  fontFamily = "Poppins",
+  fontFamily = "font-Poppins",
   backgroundColor = "#242237",
   width = "w-full",
   height = "0",
+  fontSize = "text-md",
   ...rest
 }: IBannerMktProps) => {
   const divStyle = `bg-angelBlack py-8 ${height} ${width} flex justify-center items-center`;
-  const textStyle = "text-white font-normal";
-  const font = `font-Poppins`;
+  const textStyle = `text-white font-normal ${fontSize}`;
+  const font = `${fontFamily}`;
 
   return (
     <div
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -5,21 +5,23 @@ export interface ILinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   fontFamily?: string;
   fontWeight?: "semibold" | "regular";
   textDecoration?: "none";
+  fontSize?: string;
 }
 
 const Link = ({
   color = "blue",
-  fontFamily = "Poppins",
+  fontFamily = "font-Poppins",
   fontWeight = "regular",
   textDecoration = "none",
+  fontSize = "text-md",
   ...rest
 }: ILinkProps) => {
-  const style = "no-underline transition-all duration-300 hover:opacity-60";
+  const style = `no-underline transition-all duration-300 hover:opacity-60 ${fontSize}`;
 
   //  LINK WIGHTS
   const regular = `font-normal`;
   const semibold = `font-semibold`;
-  const font = `font-Poppins`;
+  const font = `${fontFamily}`;
 
   const white = `text-white`;
   const blue = `text-angel`;
