784c4d0917a2e52bc4659f9aa7c067aa1e01083f
yuriBaza23 <yuribaza2321@gmail.com>
2022-05-30 01:14:29 -0300

    Add personal background

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,21 @@
 export const parameters = {
+  backgrounds: {
+    default: 'black',
+    values: [
+      {
+        name: 'black',
+        value: '#333333',
+      },
+      {
+        name: 'white',
+        value: '#fff',
+      },
+      {
+        name: 'gray',
+        value: '#f2f2f2',
+      },
+    ],
+  },
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
