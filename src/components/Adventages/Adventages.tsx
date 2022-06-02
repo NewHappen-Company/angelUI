34ae9cbb43339e2d2211c8efe636587a9dbf7c32
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-02 13:45:33 -0300

    💻 New adventages interface

--- src/components/Adventages/Adventages.tsx
+++ src/components/Adventages/Adventages.tsx
@@ -1,16 +1,45 @@
 import React, { HTMLAttributes } from "react";
+import IDefaultProps from "../../../@types/defaults";
 
 export interface IAdventagesProps extends HTMLAttributes<HTMLDivElement> {
   sideSize?: string;
 }
 
-const Adventages = ({ sideSize = "12", ...rest }: IAdventagesProps) => {
+type IAngelAdventagesProps = IAdventagesProps & IDefaultProps;
+
+const Adventages = ({ 
+  h,
+  w,
+  maxHeight,
+  maxWidth,
+  p,
+  px,
+  py,
+  pt,
+  pb,
+  pl,
+  pr,
+  m,
+  mx,
+  my,
+  mt,
+  mb,
+  ml,
+  mr,
+  minHeight,
+  minWidth,
+  sideSize = "12", 
+  ...rest 
+}: IAngelAdventagesProps) => {
   const style = `rounded-xl bg-gradient-to-t from-angelGrayC4 to-angelGrayE2 w-${sideSize} h-${sideSize} flex justify-center items-center`;
+
+  let defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
 
   return (
     <div
       className={`
         ${style} 
+        ${defaults}
       `}
       {...rest}
     />
