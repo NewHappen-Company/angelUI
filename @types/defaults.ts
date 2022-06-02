fba7c93822a8bca30e2d78705f44a10f3e7d2cbb
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-02 11:40:55 -0300

    💻 Defalut types

---
+++ @types/defaults.ts
@@ -0,0 +1,29 @@
+export default interface IDefaultProps {
+  w?: 'w-0' | 'w-px' | 'w-0.5' | 'w-1' | 'w-1.5' | 'w-2' | 'w-2.5' | 'w-3' | 'w-3.5' | 'w-4' | 'w-5' | 'w-6' | 'w-7' | 'w-8' | 'w-9' | 'w-10' | 'w-11' | 'w-12' | 'w-14' | 'w-16' | 'w-20' | 'w-24' | 'w-28' | 'w-32' | 'w-36' | 'w-40' | 'w-44' | 'w-48' | 'w-52' | 'w-56' | 'w-60' | 'w-64' | 'w-72' | 'w-80' | 'w-96' | 'w-full' | 'w-screen' | 'w-1/2';
+  width?: 'w-0' | 'w-px' | 'w-0.5' | 'w-1' | 'w-1.5' | 'w-2' | 'w-2.5' | 'w-3' | 'w-3.5' | 'w-4' | 'w-5' | 'w-6' | 'w-7' | 'w-8' | 'w-9' | 'w-10' | 'w-11' | 'w-12' | 'w-14' | 'w-16' | 'w-20' | 'w-24' | 'w-28' | 'w-32' | 'w-36' | 'w-40' | 'w-44' | 'w-48' | 'w-52' | 'w-56' | 'w-60' | 'w-64' | 'w-72' | 'w-80' | 'w-96' | 'w-full' | 'w-screen' | 'w-1/2';
+
+  h?: 'h-0' | 'h-px' | 'h-0.5' | 'h-1' | 'h-1.5' | 'h-2' | 'h-2.5' | 'h-3' | 'h-3.5' | 'h-4' | 'h-5' | 'h-6' | 'h-7' | 'h-8' | 'h-9' | 'h-10' | 'h-11' | 'h-12' | 'h-14' | 'h-16' | 'h-20' | 'h-24' | 'h-28' | 'h-32' | 'h-36' | 'h-40' | 'h-44' | 'h-48' | 'h-52' | 'h-56' | 'h-60' | 'h-64' | 'h-72' | 'h-80' | 'h-96' | 'h-full' | 'h-screen' | 'h-1/2';
+  height?: 'h-0' | 'h-px' | 'h-0.5' | 'h-1' | 'h-1.5' | 'h-2' | 'h-2.5' | 'h-3' | 'h-3.5' | 'h-4' | 'h-5' | 'h-6' | 'h-7' | 'h-8' | 'h-9' | 'h-10' | 'h-11' | 'h-12' | 'h-14' | 'h-16' | 'h-20' | 'h-24' | 'h-28' | 'h-32' | 'h-36' | 'h-40' | 'h-44' | 'h-48' | 'h-52' | 'h-56' | 'h-60' | 'h-64' | 'h-72' | 'h-80' | 'h-96' | 'h-full' | 'h-screen' | 'h-1/2';
+
+  minWidth?: 'min-w-0' | 'min-w-full' | 'min-w-min' | 'min-w-max' | 'min-w-fit';
+  maxWidth?: 'max-w-0' | 'max-w-none' | 'max-w-xs' | 'max-w-sm' | 'max-w-md' | 'max-w-lg' | 'max-w-xl' | 'max-w-2xl' | 'max-w-3xl' | 'max-w-4xl' | 'max-w-5xl' | 'max-w-6xl' | 'max-w-7xl' | 'max-w-full' | 'max-w-min' | 'max-w-max' | 'max-w-fit' | 'max-w-prose' | 'max-w-fit' | 'max-w-screen-sm' | 'max-w-screen-md' | 'max-w-screen-xl' | 'max-w-screen-2xl';
+
+  minHeight?: 'min-h-0' | 'min-h-full' | 'min-h-min' | 'min-h-max' | 'min-h-fit';
+  maxHeight?: 'max-h-0' | 'max-h-px' | 'max-h-0.5' | 'max-h-1' | 'max-h-1.5' | 'max-h-2' | 'max-h-2.5' | 'max-h-3' | 'max-h-3.5' | 'max-h-4' | 'max-h-5' | 'max-h-6' | 'max-h-7' | 'max-h-8' | 'max-h-9' | 'max-h-10' | 'max-h-11' | 'max-h-12' | 'max-h-14' | 'max-h-16' | 'max-h-20' | 'max-h-24' | 'max-h-28' | 'max-h-32' | 'max-h-36' | 'max-h-40' | 'max-h-44' | 'max-h-48' | 'max-h-52' | 'max-h-56' | 'max-h-60' | 'max-h-64' | 'max-h-72' | 'max-h-80' | 'max-h-96' | 'max-h-full' | 'max-h-screen' | 'max-h-min' | 'max-h-max' | 'max-h-fit';
+
+  p?: 'm-0' | 'p-px' | 'p-0.5' | 'p-1' | 'p-1.5' | 'p-2' | 'p-2.5' | 'p-3' | 'p-3.5' | 'p-4' | 'p-5' | 'p-6' | 'p-7' | 'p-8' | 'p-9' | 'p-10';
+  px?: 'px-0' | 'px-px' | 'px-0.5' | 'px-1' | 'px-1.5' | 'px-2' | 'px-2.5' | 'px-3' | 'px-3.5' | 'px-4' | 'px-5' | 'px-6' | 'px-7' | 'px-8' | 'px-9' | 'px-10';
+  py?: 'py-0' | 'py-px' | 'py-0.5' | 'py-1' | 'py-1.5' | 'py-2' | 'py-2.5' | 'py-3' | 'py-3.5' | 'py-4' | 'py-5' | 'py-6' | 'py-7' | 'py-8' | 'py-9' | 'py-10';
+  pt?: 'pt-0' | 'pt-px' | 'pt-0.5' | 'pt-1' | 'pt-1.5' | 'pt-2' | 'pt-2.5' | 'pt-3' | 'pt-3.5' | 'pt-4' | 'pt-5' | 'pt-6' | 'pt-7' | 'pt-8' | 'pt-9' | 'pt-10';
+  pr?: 'pr-0' | 'pr-px' | 'pr-0.5' | 'pr-1' | 'pr-1.5' | 'pr-2' | 'pr-2.5' | 'pr-3' | 'pr-3.5' | 'pr-4' | 'pr-5' | 'pr-6' | 'pr-7' | 'pr-8' | 'pr-9' | 'pr-10';
+  pb?: 'pb-0' | 'pb-px' | 'pb-0.5' | 'pb-1' | 'pb-1.5' | 'pb-2' | 'pb-2.5' | 'pb-3' | 'pb-3.5' | 'pb-4' | 'pb-5' | 'pb-6' | 'pb-7' | 'pb-8' | 'pb-9' | 'pb-10';
+  pl?: 'pl-0' | 'pl-px' | 'pl-0.5' | 'pl-1' | 'pl-1.5' | 'pl-2' | 'pl-2.5' | 'pl-3' | 'pl-3.5' | 'pl-4' | 'pl-5' | 'pl-6' | 'pl-7' | 'pl-8' | 'pl-9' | 'pl-10';
+
+  m?: 'm-0' | 'm-px' | 'm-0.5' | 'm-1' | 'm-1.5' | 'm-2' | 'm-2.5' | 'm-3' | 'm-3.5' | 'm-4' | 'm-5' | 'm-6' | 'm-7' | 'm-8' | 'm-9' | 'm-10' | 'm-auto';
+  mx?: 'mx-0' | 'mx-px' | 'mx-0.5' | 'mx-1' | 'mx-1.5' | 'mx-2' | 'mx-2.5' | 'mx-3' | 'mx-3.5' | 'mx-4' | 'mx-5' | 'mx-6' | 'mx-7' | 'mx-8' | 'mx-9' | 'mx-10' | 'mx-auto';
+  my?: 'my-0' | 'my-px' | 'my-0.5' | 'my-1' | 'my-1.5' | 'my-2' | 'my-2.5' | 'my-3' | 'my-3.5' | 'my-4' | 'my-5' | 'my-6' | 'my-7' | 'my-8' | 'my-9' | 'my-10' | 'my-auto';
+  mt?: 'mt-0' | 'mt-px' | 'mt-0.5' | 'mt-1' | 'mt-1.5' | 'mt-2' | 'mt-2.5' | 'mt-3' | 'mt-3.5' | 'mt-4' | 'mt-5' | 'mt-6' | 'mt-7' | 'mt-8' | 'mt-9' | 'mt-10';
+  mr?: 'mr-0' | 'mr-px' | 'mr-0.5' | 'mr-1' | 'mr-1.5' | 'mr-2' | 'mr-2.5' | 'mr-3' | 'mr-3.5' | 'mr-4' | 'mr-5' | 'mr-6' | 'mr-7' | 'mr-8' | 'mr-9' | 'mr-10';
+  mb?: 'mb-0' | 'mb-px' | 'mb-0.5' | 'mb-1' | 'mb-1.5' | 'mb-2' | 'mb-2.5' | 'mb-3' | 'mb-3.5' | 'mb-4' | 'mb-5' | 'mb-6' | 'mb-7' | 'mb-8' | 'mb-9' | 'mb-10';
+  ml?: 'ml-0' | 'ml-px' | 'ml-0.5' | 'ml-1' | 'ml-1.5' | 'ml-2' | 'ml-2.5' | 'ml-3' | 'ml-3.5' | 'ml-4' | 'ml-5' | 'ml-6' | 'ml-7' | 'ml-8' | 'ml-9' | 'ml-10';
+}
