06405093f7fb088c36130dcf531040d7416017db
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-02 18:21:40 -0300

    🚧 Maintenance

---
+++ index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from "./src/components/Button";
+export { default as InputSimulator } from "./src/components/InputSimulator";
+export { default as Input } from "./src/components/Input";
+export { default as BannerMkt } from "./src/components/BannerMkt";
+export { default as Link } from "./src/components/Link";
+export { default as Row } from "./src/components/Row";
+export { default as Column } from "./src/components/Column";
--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ const packageJson = require("./package.json");
 
 export default [
   {
-    input: "src/index.ts",
+    input: "./index.ts",
     output: [
       {
         file: packageJson.main,
