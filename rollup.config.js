533a78e4788e064d48ef0e244c7ff233cd60332a
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-03 12:28:19 -0300

    🚧 Maintenance error hooks

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
+import external from 'rollup-plugin-peer-deps-external'
 
 const packageJson = require("./package.json");
 
@@ -22,6 +23,7 @@ export default [
       },
     ],
     plugins: [
+      external('./package.json'),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
