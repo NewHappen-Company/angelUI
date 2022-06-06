176c4bc282652a65ae6e6490b4e671f8404f2467
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-06 17:09:04 -0300

    💻 Call Toast Context Provider

---
+++ src/components/Toast/TestToStories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ToastContextProvider } from "../../contexts/src/ToastContext";
+import Foo from "./Foo";
+
+export default function App() {
+
+  return (
+    <ToastContextProvider>
+      <Foo/>
+    </ToastContextProvider>
+  );
+}
