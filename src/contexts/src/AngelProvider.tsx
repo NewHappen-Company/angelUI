12f667bf8c90b86bff5091c5c596cd91313529ca
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-08 01:29:16 -0300

    💻 Create AngelProvider

---
+++ src/contexts/src/AngelProvider.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+import { ToastContextProvider } from './ToastContext';
+
+interface AngelProviderProps {
+  children: ReactNode;
+}
+
+const AngelProvider = ({children}: AngelProviderProps) => {
+  return (
+    <ToastContextProvider>
+      {children}
+    </ToastContextProvider>
+  );
+}
+
+export default AngelProvider;
