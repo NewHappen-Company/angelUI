51a0b7e76b3afc9340a3871c528a8380c643ae9e
yuriBaza23 <yuribaza2321@gmail.com>
2022-06-09 19:08:52 -0300

    :computer: - feat: add UseToast in the main file
    UseToast added in index.ts to use

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
-export { default as Button } from "./src/components/Button";
-export { default as InputSimulator } from "./src/components/InputSimulator";
-export { default as Input } from "./src/components/Input";
-export { default as BannerMkt } from "./src/components/BannerMkt";
-export { default as Link } from "./src/components/Link";
-export { default as Row } from "./src/components/Row";
-export { default as Column } from "./src/components/Column";
-export { default as Accordion } from "./src/components/Accordion";
-export { default as Toast } from "./src/components/Toast";
+export { default as Button } from './src/components/Button';
+export { default as InputSimulator } from './src/components/InputSimulator';
+export { default as Input } from './src/components/Input';
+export { default as BannerMkt } from './src/components/BannerMkt';
+export { default as Link } from './src/components/Link';
+export { default as Row } from './src/components/Row';
+export { default as Column } from './src/components/Column';
+export { default as Accordion } from './src/components/Accordion';
+export { default as Toast } from './src/components/Toast';
 // ----------- CONTEXTS -----------
-export { default as AngelProvider } from "./src/contexts/src/AngelProvider";
+export { default as AngelProvider } from './src/contexts/src/AngelProvider';
+export { default as UseToast } from './src/contexts/src/ToastContext';
