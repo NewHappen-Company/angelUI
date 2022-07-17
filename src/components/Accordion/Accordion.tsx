0bdac9c6efdcf0a44a2e67f43c240cc246f5a3fe
yuriBaza23 <yuribaza2321@gmail.com>
2022-07-17 15:26:00 -0300

    refactor: line

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,8 +1,8 @@
-import React, { Children, HTMLAttributes, ReactNode, useRef } from "react";
-import Button from "../Button";
-import { BsChevronDown } from "react-icons/bs";
-import IDefaultProps from "../../../@types/defaults";
-import { Property } from "csstype";
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { HTMLAttributes, ReactNode, useRef } from 'react';
+import { BsChevronDown } from 'react-icons/bs';
+import Button from '../Button';
+import IDefaultProps from '../../../@types/defaults';
 
 export interface IAccordionProps extends HTMLAttributes<HTMLDivElement> {
   text?: ReactNode;
@@ -10,14 +10,14 @@ export interface IAccordionProps extends HTMLAttributes<HTMLDivElement> {
   color?: string;
   titleFontSize?: string;
   textFontSize?: string;
-  border?: "border-b" | "border-0";
+  border?: 'border-b' | 'border-0';
 }
 
 type IAngelAccordionProps = IAccordionProps & IDefaultProps;
 
 const Accordion = ({
-  h = "h-full",
-  w = "w-full",
+  h = 'h-full',
+  w = 'w-full',
   maxHeight,
   maxWidth,
   p,
@@ -36,10 +36,10 @@ const Accordion = ({
   mr,
   minHeight,
   minWidth,
-  color = "text-white",
-  titleFontSize = "text-2xl",
-  textFontSize = "text-dm",
-  border = "border-b",
+  color = 'text-white',
+  titleFontSize = 'text-2xl',
+  textFontSize = 'text-dm',
+  border = 'border-b',
   children,
   text,
   ...rest
@@ -47,14 +47,14 @@ const Accordion = ({
   const [click, setClick] = React.useState(false);
 
   const chevron = click
-    ? `transition-transform duration-300 transform rotate-180`
-    : `transition-transform duration-300 transform`;
+    ? 'transition-transform duration-300 transform rotate-180'
+    : 'transition-transform duration-300 transform';
 
   const style = `${border} border-solid ${color} `;
   const titleDivstyle = `flex justify-between items-center font-bold ${titleFontSize} flex justify-between`;
   const textDivStyle = `${textFontSize} pt-2 px-2 text-justify overflow-hidden max-h-0 transition-all duration-300 flex mb-2`;
 
-  let defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
+  const defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
 
   const handleClick = () => {
     setClick(!click);
@@ -62,7 +62,6 @@ const Accordion = ({
 
   const refTab = useRef<HTMLDivElement>(null);
   const tabHeight = refTab.current?.scrollHeight;
-  console.log(tabHeight);
 
   return (
     <div
@@ -75,13 +74,13 @@ const Accordion = ({
       <div className={titleDivstyle} {...rest}>
         {text}
         <Button btnType="text" shape="circle" onClick={handleClick}>
-          <BsChevronDown style={{ fill: "white" }} className={chevron} />
+          <BsChevronDown style={{ fill: 'white' }} className={chevron} />
         </Button>
       </div>
       <div
         ref={refTab}
         className={textDivStyle}
-        style={{ maxHeight: click ? `${tabHeight}px` : "10px" }}
+        style={{ maxHeight: click ? `${tabHeight}px` : '10px' }}
         {...rest}
       >
         {children}
