22af00dcc436520a6888233bbe9328695b1e41d5
yuriBaza23 <yuribaza2321@gmail.com>
2022-07-17 15:30:21 -0300

    feat: link

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,11 +1,11 @@
-import React, { AnchorHTMLAttributes } from "react";
-import IDefaultProps from "../../../@types/defaults";
+import React, { AnchorHTMLAttributes } from 'react';
+import IDefaultProps from '../../../@types/defaults';
 import '../../styles/angel';
 
 export interface ILinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
-  color?: "white" | "blue";
+  color?: 'white' | 'blue' | string;
   fontFamily?: string;
-  fontWeight?: "semibold" | "regular";
+  fontWeight?: 'semibold' | 'regular';
   textDecoration?: string;
   fontSize?: string;
 }
@@ -33,23 +33,22 @@ const Link = ({
   mr,
   minHeight,
   minWidth,
-  color = "blue",
-  fontFamily = "font-Poppins",
-  fontWeight = "regular",
-  textDecoration = "no-underline",
-  fontSize = "text-md",
+  color = 'blue',
+  fontFamily = 'font-Poppins',
+  fontWeight = 'regular',
+  fontSize = 'text-md',
   ...rest
 }: IAngelLinkProps) => {
   const style = `no-underline transition-all duration-300 hover:opacity-60 ${fontSize}`;
 
   //  LINK WIGHTS
-  const regular = `font-normal`;
-  const semibold = `font-semibold`;
+  const regular = 'font-normal';
+  const semibold = 'font-semibold';
 
-  const white = `text-white`;
-  const blue = `text-angel`;
+  const white = 'text-white';
+  const blue = 'text-angel';
 
-  let defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
+  const defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
 
   return (
     <a
@@ -57,17 +56,19 @@ const Link = ({
         ${style} 
         ${fontFamily}
         ${
-          fontWeight === "regular"
+          fontWeight === 'regular'
             ? regular
-            : fontWeight === "semibold"
-            ? semibold
-            : ""
+            : fontWeight === 'semibold'
+              ? semibold
+              : ''
         }
-        ${color === "white" ? white : color === "blue" ? blue : ""}
+        ${color === 'white' ? white : color === 'blue' ? blue : color}
         ${defaults}
       `}
       {...rest}
-    />
+    >
+      {rest.children}
+    </a>
   );
 };
 
