b895cd0b0bc2df685da864dcf936795f5b3f64fc
yuriBaza23 <yuribaza2321@gmail.com>
2022-07-17 15:29:15 -0300

    fix: styles bug icon

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,5 +1,5 @@
-import React, { ButtonHTMLAttributes } from "react";
-import IDefaultProps from "../../../@types/defaults";
+import React, { ButtonHTMLAttributes } from 'react';
+import IDefaultProps from '../../../@types/defaults';
 
 export interface IIconButtonProps
   extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -8,7 +8,7 @@ export interface IIconButtonProps
 
 type IAngelIconButtonProps = IIconButtonProps & IDefaultProps;
 
-const IconButton = ({ 
+const IconButton = ({
   h,
   w,
   maxHeight,
@@ -27,14 +27,15 @@ const IconButton = ({
   mb,
   ml,
   mr,
+  bg,
   minHeight,
   minWidth,
-  sideSize = "12", 
+  sideSize = '12',
   ...rest
 }: IAngelIconButtonProps) => {
-  const style = `h-${sideSize} w-${sideSize} flex justify-center items-center rounded-md bg-angelBlack2 pointer transition-all duration-300 hover:opacity-75`;
+  const style = `h-${sideSize} w-${sideSize} flex justify-center items-center rounded-md ${bg || 'bg-angelBlack2'} pointer transition-all duration-300 hover:opacity-75`;
 
-  let defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
+  const defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
 
   return (
     <button
