cc00bc8aba22c05e295861f0693e1eea47e3b279
yuriBaza23 <yuribaza2321@gmail.com>
2022-07-17 15:27:04 -0300

    fix: align items

--- src/components/BannerMkt/BannerMkt.tsx
+++ src/components/BannerMkt/BannerMkt.tsx
@@ -1,9 +1,9 @@
-import React, { HTMLAttributes } from "react";
-import IDefaultProps from "../../../@types/defaults";
+import React, { HTMLAttributes } from 'react';
+import IDefaultProps from '../../../@types/defaults';
 import '../../styles/angel';
 
 export interface IBannerMktProps extends HTMLAttributes<HTMLDivElement> {
-  backgroundColor?: "#242237";
+  backgroundColor?: '#242237';
   fontFamily?: string;
   fontSize?: string;
 }
@@ -11,8 +11,8 @@ export interface IBannerMktProps extends HTMLAttributes<HTMLDivElement> {
 type IAngelBannerMktProps = IBannerMktProps & IDefaultProps;
 
 const BannerMkt = ({
-  w = "w-full",
-  h = "h-0",
+  w = 'w-full',
+  h = 'h-0',
   maxHeight,
   maxWidth,
   p,
@@ -31,20 +31,20 @@ const BannerMkt = ({
   mr,
   minHeight,
   minWidth,
-  fontFamily = "font-Poppins",
-  backgroundColor = "#242237",
-  fontSize = "text-md",
+  fontFamily = 'font-Poppins',
+  fontSize = 'text-md',
   ...rest
 }: IAngelBannerMktProps) => {
-  const divStyle = `bg-angelBlack py-8 ${h} ${w} flex justify-center items-center`;
+  const divStyle = `flex justify-center items-center bg-angelBlack py-8 ${h} ${w}`;
   const textStyle = `text-white font-normal ${fontSize}`;
 
-  let defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
+  const defaults = `${w} ${h} ${maxWidth} ${maxHeight} ${minWidth} ${minHeight} ${minWidth} ${minHeight} ${p} ${px} ${py} ${pt} ${pb} ${pl} ${pr} ${m} ${mx} ${my} ${mt} ${mb} ${ml} ${mr}`;
 
   return (
     <div
       className={`
         ${divStyle} 
+        ${defaults}
       `}
       {...rest}
     >
@@ -52,10 +52,11 @@ const BannerMkt = ({
         className={`
       ${textStyle} 
       ${fontFamily}
-      ${defaults}
     `}
         {...rest}
-      />
+      >
+        {rest.children}
+      </h3>
     </div>
   );
 };
