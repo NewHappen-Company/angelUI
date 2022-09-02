3a2b5996b9e393dad852c7300c3e720632d8d405
yuriBaza23 <yuribaza2321@gmail.com>
2022-09-02 14:24:35 -0300

    feat: add color

--- @types/colors.ts
+++ @types/colors.ts
@@ -1,3 +1,6 @@
 export const angelBlue = '#439EFF';
 export const angelRed = '#ff434c';
 export const angelBlackP5 = 'rgba(18, 71, 128, 0.05)';
+export const angelGray = '#7E7E7E';
+export const angelGray100 = '#272727';
+export const angelGrayP65 = 'rgba(243, 246, 249, 0.65)';
